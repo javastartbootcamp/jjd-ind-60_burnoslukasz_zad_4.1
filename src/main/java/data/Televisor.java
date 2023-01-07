c13e622c95aae8926a9ad530b442d1a793b5474c
Łukasz Burnos <burnoslukasz@gmail.com>
2023-01-07 12:52:28 +0100

    zadanie_4.1 do sprawdzenia, poprawka

--- src/main/java/data/Televisor.java
+++ src/main/java/data/Televisor.java
@@ -1,3 +1,5 @@
+package data;
+
 public class Televisor {
     boolean tv;
 
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,3 +1,7 @@
+package pl.javastart.task;
+
+import data.Televisor;
+
 public class Main {
     public static void main(String[] args) {
         Televisor televisor1 = new Televisor(false);
